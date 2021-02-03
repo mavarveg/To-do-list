8bfb007a68f6db4fa40d9eca36ce185c8e857754
mavarveg <mavarg07@gmail.com>
2021-02-03 00:10:26 -0600

    add docs

---
+++ docs/precache-manifest.1f0f4c61b9f1921712bcda0f4b8f1de9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1563a5f423ae80663c0bd41c82573bd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "111a3c8f9de2aa0c829c",
+    "url": "/static/js/2.a5ce1fe5.chunk.js"
+  },
+  {
+    "revision": "d5478227434978c10ea4",
+    "url": "/static/js/main.a363b2e6.chunk.js"
+  },
+  {
+    "revision": "39d97d9304d160eeba04",
+    "url": "/static/js/runtime-main.1363369d.js"
+  }
+]);
